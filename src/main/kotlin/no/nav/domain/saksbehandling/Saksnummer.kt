f95fe42a984eda399e6a211bf83368f9eb4034a1
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-02-02 11:06:11 +0100

    Lagt til manglende data på class

--- src/main/kotlin/no/nav/domain/saksbehandling/Saksnummer.kt
+++ src/main/kotlin/no/nav/domain/saksbehandling/Saksnummer.kt
@@ -2,7 +2,7 @@ package no.nav.domain.saksbehandling
 
 import no.nav.domain.felles.Verdiobjekt
 
-class Saksnummer(override val verdi: String) : Verdiobjekt<String> {
+data class Saksnummer(override val verdi: String) : Verdiobjekt<String> {
 
     init {
         require(verdi.matches(SEVEN_DIGITS_REGEX)) { "$verdi tilfredstiller ikke kravet til et gyldig saksnummer" }
