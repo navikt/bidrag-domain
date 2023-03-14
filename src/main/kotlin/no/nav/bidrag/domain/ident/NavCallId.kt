687f0a13c739c1c52bf68d94330b7efefeb46a7d
Bent J. Lorentzen <bent.lorentzen@nav.no>
2023-03-14 15:03:00 +0100

    Endret til String

--- src/main/kotlin/no/nav/bidrag/domain/ident/NavCallId.kt
+++ src/main/kotlin/no/nav/bidrag/domain/ident/NavCallId.kt
@@ -3,19 +3,19 @@ package no.nav.bidrag.domain.ident
 import jakarta.persistence.AttributeConverter
 import no.nav.bidrag.domain.felles.Verdiobjekt
 import org.springframework.core.convert.converter.Converter
-import java.util.UUID
+import java.util.String
 
-data class NavCallId(override val verdi: UUID) : Verdiobjekt<UUID>
+data class NavCallId(override val verdi: String) : Verdiobjekt<String>
 
 class NavCallIdReadingConverter : Converter<String, NavCallId> {
-    override fun convert(source: String) = NavCallId(UUID.fromString(source))
+    override fun convert(source: String) = NavCallId(source)
 }
 
 class NavCallIdWritingConverter : Converter<NavCallId, String> {
-    override fun convert(source: NavCallId) = source.verdi.toString()
+    override fun convert(source: NavCallId) = source.verdi
 }
 
-class NavCallIdConverter : AttributeConverter<NavCallId, UUID> {
-    override fun convertToEntityAttribute(source: UUID?) = source?.let { NavCallId(source) }
+class NavCallIdConverter : AttributeConverter<NavCallId, String> {
+    override fun convertToEntityAttribute(source: String?) = source?.let { NavCallId(source) }
     override fun convertToDatabaseColumn(source: NavCallId?) = source?.verdi
 }
