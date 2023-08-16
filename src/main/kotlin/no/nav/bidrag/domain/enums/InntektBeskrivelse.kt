0ffddfc076a3b6551def824e6f97a36918caddd8
Lars Otto Haugen <lars.otto.haugen@nav.no>
2023-08-16 11:15:27 +0200

    Justert litt på enum InntektBeskrivelse

--- src/main/kotlin/no/nav/bidrag/domain/enums/InntektBeskrivelse.kt
+++ src/main/kotlin/no/nav/bidrag/domain/enums/InntektBeskrivelse.kt
@@ -1,9 +1,9 @@
 package no.nav.bidrag.domain.enums
 
-enum class InntektBeskrivelse(verdi: String) {
-    AINNTEKT_BEREGNET_3MND("Ainntekt beregnet inntekt siste 3 mnd"),
-    AINNTEKT_BEREGNET_12MND("Ainntekt beregnet inntekt siste 12 mnd"),
-    AINNTEKT("Ainntekt"),
+enum class InntektBeskrivelse(val visningsnavn: String) {
+    AINNTEKT_BEREGNET_3MND("Lønn og trekk siste 3 mnd"),
+    AINNTEKT_BEREGNET_12MND("Lønn og trekk siste 12 mnd"),
+    AINNTEKT("Lønn og trekk"),
     LIGNINGSINNTEKT("Sigrun ligningsinntekt (LIGS)"),
     KAPITALINNTEKT("Sigrun kapitalinntekt (KAPS)"),
     UTVIDET_BARNETRYGD("Utvidet barnetrygd"),
