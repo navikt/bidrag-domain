944be40cfb540fb2518e7a443af0cf9518758a50
LarsOttoHaugen <lars.otto.haugen@nav.no>
2023-09-25 13:42:44 +0200

    Nye og endrede enums relatert til bidrag-inntekt

--- src/main/kotlin/no/nav/bidrag/domain/enums/InntektKategori.kt
+++ src/main/kotlin/no/nav/bidrag/domain/enums/InntektKategori.kt
@@ -2,25 +2,75 @@ package no.nav.bidrag.domain.enums
 
 enum class InntektKategori(
     val visningsnavn: String,
-    val inntektTypeListe: List<InntektType>
+    val bestårAvInntektTypeListe: List<InntektType>,
+    val tilhørerInntektGruppe: InntektGruppe
 ) {
-    AINNTEKT_BEREGNET_3MND(
-        "Lønn og trekk siste 3 mnd",
-        listOf(InntektType.ALOYSE, InntektType.ATTFORING_AAP, InntektType.PENSJON, InntektType.SYKEPENGER)
+    AINNTEKT(
+        visningsnavn = "Lønn og trekk",
+        bestårAvInntektTypeListe = listOf(
+            InntektType.DAGPENGER,
+            InntektType.AAP,
+            InntektType.FØDSEL_ADOPSJON,
+            InntektType.LØNN_TREKK,
+            InntektType.PENSJON,
+            InntektType.SYKEPENGER
+        ),
+        tilhørerInntektGruppe = InntektGruppe.PERSONINNTEKT
     ),
-    AINNTEKT_BEREGNET_12MND(
-        "Lønn og trekk siste 12 mnd",
-        listOf(InntektType.ALOYSE, InntektType.ATTFORING_AAP, InntektType.PENSJON, InntektType.SYKEPENGER)
-    ),
-    AINNTEKT("Lønn og trekk", listOf(InntektType.ALOYSE, InntektType.ATTFORING_AAP, InntektType.PENSJON, InntektType.SYKEPENGER)),
-    LIGNINGSINNTEKT("Sigrun ligningsinntekt (LIGS)", listOf(InntektType.LIGNING_SKE)),
-    KAPITALINNTEKT("Sigrun kapitalinntekt (KAPS)", listOf(InntektType.KAPITALINNTEKT_SKE)),
-    OVERGANGSSTØNAD("Overgangsstønad", listOf(InntektType.OVERGANGSSTONAD)),
-    OVERGANGSSTØNAD_BEREGNET_3MND("Overgangsstønad siste 3 mnd", listOf(InntektType.OVERGANGSSTONAD)),
-    OVERGANGSSTØNAD_BEREGNET_12MND("Overgangsstønad siste 12 mnd", listOf(InntektType.OVERGANGSSTONAD));
 
-    companion object {
-        fun finnInntektkategorierSomInneholderInntektType(inntektType: InntektType) =
-            InntektKategori.values().filter { inntektType in it.inntektTypeListe }
-    }
+    AINNTEKT_BEREGNET_3MND(
+        visningsnavn = "Lønn og trekk siste 3 mnd",
+        bestårAvInntektTypeListe = listOf(
+            InntektType.DAGPENGER,
+            InntektType.AAP,
+            InntektType.FØDSEL_ADOPSJON,
+            InntektType.LØNN_TREKK,
+            InntektType.PENSJON,
+            InntektType.SYKEPENGER
+        ),
+        tilhørerInntektGruppe = InntektGruppe.PERSONINNTEKT
+    ),
+
+    AINNTEKT_BEREGNET_12MND(
+        visningsnavn = "Lønn og trekk siste 12 mnd",
+        bestårAvInntektTypeListe = listOf(
+            InntektType.DAGPENGER,
+            InntektType.AAP,
+            InntektType.FØDSEL_ADOPSJON,
+            InntektType.LØNN_TREKK,
+            InntektType.PENSJON,
+            InntektType.SYKEPENGER
+        ),
+        tilhørerInntektGruppe = InntektGruppe.PERSONINNTEKT
+    ),
+
+    LIGNINGSINNTEKT(
+        visningsnavn = "Sigrun ligningsinntekt (LIGS)",
+        bestårAvInntektTypeListe = listOf(InntektType.SKATTEGRUNNLAG_SKE),
+        tilhørerInntektGruppe = InntektGruppe.PERSONINNTEKT
+    ),
+
+    KAPITALINNTEKT(
+        visningsnavn = "Sigrun kapitalinntekt (KAPS)",
+        bestårAvInntektTypeListe = listOf(InntektType.KAPITALINNTEKT_SKE),
+        tilhørerInntektGruppe = InntektGruppe.KAPITALINNTEKT
+    ),
+
+    OVERGANGSSTØNAD(
+        visningsnavn = "Overgangsstønad",
+        bestårAvInntektTypeListe = listOf(InntektType.OVERGANGSSTØNAD),
+        tilhørerInntektGruppe = InntektGruppe.INGEN
+    ),
+
+    OVERGANGSSTØNAD_BEREGNET_3MND(
+        visningsnavn = "Overgangsstønad siste 3 mnd",
+        bestårAvInntektTypeListe = listOf(InntektType.OVERGANGSSTØNAD),
+        tilhørerInntektGruppe = InntektGruppe.INGEN
+    ),
+
+    OVERGANGSSTØNAD_BEREGNET_12MND(
+        visningsnavn = "Overgangsstønad siste 12 mnd",
+        bestårAvInntektTypeListe = listOf(InntektType.OVERGANGSSTØNAD),
+        tilhørerInntektGruppe = InntektGruppe.INGEN
+    );
 }
